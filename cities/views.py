d6adabebbe256807a294bfdfce4591898a80b2e5
Kurmashev.A.V <noolfon@yandex.ru>
2021-02-01 23:09:25 +0300

    added forms

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from cities.forms import CityForm
+from cities.models import City
 
-# Create your views here.
+__all__ = (
+    'home', 'CityDetailView',
+)
+
+
+def home(request):
+    if request.POST:
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+    form = CityForm()
+    qs = City.objects.all()
+    return render(request, 'cities/home.html', {'cities': qs, 'form': form })
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
